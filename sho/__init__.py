9ea1363982e9133a6fe3298c1906ac2b49b78936
davewd <davewd@me.com>
2019-12-23 21:56:28 +1000

    tryng something new

--- sho/__init__.py
+++ sho/__init__.py
@@ -5,3 +5,12 @@
 __author__ = """Dave Dawson"""
 __email__ = 'davedawson.co@gmail.com'
 __version__ = '0.1.0'
+
+"""Main module."""
+def w(obj, output_type=OUTPUT_TYPE.DEFAULT.value):
+    """
+    Main function called, defaults to 
+        :param obj: the variable to show
+        :param output_type=OUTPUT_TYPE.DEFAULT.value: ability to force output types under certain scenarios
+    """
+    output(obj,output_type);
